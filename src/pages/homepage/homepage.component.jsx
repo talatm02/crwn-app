b7e5a647b9e3f855c911acea79a141aec698c5ff
talat <talatm02@gmail.com>
2019-08-23 22:04:36 +0400

    styling done

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component'
+const HomePage = (props) => {
+    return (
+        <div className="homepage">
+            <Directory></Directory>
+        </div>
+    )
+}
+
+export default HomePage;
