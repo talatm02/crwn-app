dd69a9ec7098af25052b717fb69030c3c493369d
talat <talatm02@gmail.com>
2019-09-01 00:09:38 +0400

    cart quantity update code

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -4,4 +4,9 @@ const toggleCartHidden = () => ({
     type: CartTypes.TOGGLE_CART_HIDDEN
 });
 
+export const addItem = item => ({
+    type: CartTypes.ADD_ITEM,
+    payload: item
+})
+
 export default toggleCartHidden;
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+const addItemToCart = (cartItems, itemToCart) => {
+    const existingItem = cartItems.find(item => {
+        return (item.id === itemToCart.id)
+    });
+
+    if (existingItem) {
+        return cartItems.map(item => 
+             ((item.id === itemToCart.id) ?{ ...item,
+                quantity: item.quantity + 1}
+            : item)
+        )
+    }
+    return [...cartItems, { ...itemToCart, quantity: 1 }]
+}
+
+export default addItemToCart;
